00a8e43fbf4d81821a4553a8f9cb0375440e8c51
Cássio Oliveira Da Silva <nit0279619@aluno.firjansenaisesi.com.br>
2026-07-06 19:33:33 -0300

    estrutura inicial do back-end

---
+++ back-end/src/main/java/br/senai/gestaoservicos/api/entity/OrdemServico.java
@@ -0,0 +1,5 @@
+package br.senai.gestaoservicos.api.entity;
+
+public class OrdemServico {
+
+}
---
+++ back-end/src/test/java/dev/gestao/servicos/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gestao.servicos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
